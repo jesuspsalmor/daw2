030c888623396bd57e4c03f2130ad7bf632cddae
jesuspsalmor <jesusp.salmor@educa.jcyl.es>
2025-01-31 19:33:22 +0100

    cambios

---
+++ cliente/tarea045/app.js
@@ -0,0 +1,10 @@
+document.getElementById("formEnviar").addEventListener("click",()=>{
+    const nombre=document.getElementById("nombre").value;
+    const email=document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    const password2=document.getElementById("password2").value;
+    const edad=document.getElementById("edad").value;
+    const sexo=document.
+
+    
+})
